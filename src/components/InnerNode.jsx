08f960fe0435f04eea042e2f6efa69a2d624754a
Racer <racer@example.com>
2026-01-24 16:04:11 +0300

    Add context menu with Edit and Delete options to InnerNode component

--- src/components/InnerNode.jsx
+++ src/components/InnerNode.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Handle, Position } from "reactflow";
+import { Button } from "primereact/button";
+import { Menu } from "primereact/menu";
 import { NODE } from "../config/nodeConfig";
 
 // Компонент вложенной ноды с уникальным ID
-const InnerNode = ({ data, id }) => {
+const InnerNode = ({ data, id, parentNode }) => {
+  const menu = useRef(null);
+  const menuBtn = useRef(null);
+
+  const handleMenuToggle = (e) => {
+    e.stopPropagation();
+    menu.current.toggle(e);
+  };
+
+  const handleEdit = () => {
+    console.log("Редактировать ноду", id);
+  };
+
+  const handleDelete = () => {
+    if (parentNode && typeof window.deleteNodeFromGroup === 'function') {
+      window.deleteNodeFromGroup(id);
+    }
+  };
+
+  const menuItems = [
+    {
+      label: "Редактировать",
+      icon: "pi pi-pencil",
+      command: handleEdit,
+    },
+    {
+      label: "Удалить",
+      icon: "pi pi-trash",
+      command: handleDelete,
+    },
+  ];
+
   return (
     <div
       style={{
@@ -17,6 +50,7 @@ const InnerNode = ({ data, id }) => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        position: "relative",
       }}
     >
       <div>{data.label}</div>
@@ -33,6 +67,22 @@ const InnerNode = ({ data, id }) => {
           borderColor: NODE.handleBorderColor,
         }}
       />
+      <Button
+        ref={menuBtn}
+        icon="pi pi-ellipsis-v"
+        className="p-button-text p-button-plain"
+        size="small"
+        style={{
+          position: "absolute",
+          top: -20,
+          right: 0,
+          width: "auto",
+          paddingLeft: 4,
+          paddingRight: 4,
+        }}
+        onClick={handleMenuToggle}
+      />
+      <Menu ref={menu} model={menuItems} popup />
     </div>
   );
 };
