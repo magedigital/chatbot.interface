4cc0c4ced9864b6bfd35662eadce7fdc0e0a04c8
Racer <racer@example.com>
2026-02-03 20:27:55 +0300

    feat: Configure Vite to use homepage value from package.json as base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+
+// Читаем package.json для получения значения homepage
+const packageJson = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8'));
+const homepage = packageJson.homepage || '/';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: homepage,
   plugins: [react()],
 })
