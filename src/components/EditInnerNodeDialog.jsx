2a1d55ed3dfe6a1f86850654707b122861531d92
Racer <racer@example.com>
2026-02-03 17:41:36 +0300

    feat: Restrict Editor toolbar to specified commands only

--- src/components/EditInnerNodeDialog.jsx
+++ src/components/EditInnerNodeDialog.jsx
@@ -265,20 +265,6 @@ const EditInnerNodeDialog = ({ visible, onHide, onSave, data }) => {
           headerTemplate={header}
           ptOptions={toolbarOptions}
           modules={{
-            toolbar: [
-              [{ header: [1, 2, 3, 4, 5, 6, false] }],
-              [{ size: ["small", false, "large", "huge"] }],
-              ["bold", "italic", "underline", "strike"],
-              [{ color: [] }, { background: [] }],
-              [{ script: "sub" }, { script: "super" }],
-              ["blockquote", "code-block"],
-              [{ list: "ordered" }, { list: "bullet" }],
-              [{ indent: "-1" }, { indent: "+1" }],
-              [{ direction: "rtl" }],
-              [{ align: [] }],
-              ["link", "image", "video"],
-              ["clean"]
-            ],
             // Настройка для кастомизации диалога ссылки
             clipboard: {
               matchVisual: false
