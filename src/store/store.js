32b30d7be175a0f9177d1ff86b588360da4364b2
Racer <racer@example.com>
2026-02-16 13:17:28 +0300

    feat: add action logging middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,24 @@ import nodesReducer from "./nodesSlice";
 import configReducer from "./configSlice";
 import editorReducer from "./editorSlice";
 
+// Middleware для логирования экшнов
+const logger = (store) => (next) => (action) => {
+  console.group(`%cRedux Action: ${action.type}`, 'color: #03A9F4; font-weight: bold;');
+  console.log('%cPrevious State:', 'color: #9E9E9E; font-weight: bold;', store.getState());
+  console.log('%cAction:', 'color: #00C853; font-weight: bold;', action);
+  const result = next(action);
+  console.log('%cNext State:', 'color: #FF9800; font-weight: bold;', store.getState());
+  console.groupEnd();
+  return result;
+};
+
 export const store = configureStore({
   reducer: {
     nodes: nodesReducer,
     config: configReducer,
     editor: editorReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
